bf626464f47679978d87cccf82aba49ab0c888ea
Ayush Niraula <ayush.niraula@office.mypay.com.np>
2024-11-29 10:51:40 +0545

    Added : Design files and seperating components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './assets/css/tailwind.css';
+import './assets/css/main.css';
+import Sidebar from './components/Sidebar';
+import Match from './components/Match';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import One from './components/One';
+import Two from './components/Two';
 
 function App() {
 
   return (
-    <></>
+    <>
+      <Sidebar />
+      <div id="wrapper">
+        <Match />
+        <One />
+        <Two />
+        <Contact />
+      </div>
+      <Footer />
+    </>
   )
 }
 
 export default App
+
